1c0f57db3c8dd8fed0c3c331610ddd23aa70153a
hiro9108 <hiroshi.8.egawa@gmail.com>
2021-01-18 22:29:07 -0800

    Add layout

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,12 +2,14 @@ import "../sass/main.scss";
 
 import Header from "./Header/Header";
 import Main from "./Main/Main";
+import Footer from "./Footer/Footer";
 
 const App = () => {
   return (
     <>
       <Header />
       <Main />
+      <Footer />
     </>
   );
 };
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useLayoutEffect, useRef } from "react";
+
+import Navbar from "./Navbar/Navbar";
 
 const Header = () => {
+  // Todo check later
+  const wrapRef = useRef();
+  const blockRef = useRef();
+
+  useLayoutEffect(() => {
+    // console.log(wrapRef.current);
+    // for (let i = 0; i < 400; i++) {
+    //   wrapRef.current.innerHTML += `<div class="header--wrap--block" ref={blockRef}/>`;
+    //   blockRef.current.style.animationDelay = `${i * 0.05}s`;
+    // }
+    // console.log(blockRef.current);
+    const wrap = document.getElementsByClassName("header--wrap");
+    const blocks = document.getElementsByClassName("header--wrap--block");
+    for (let i = 1; i < 400; i++) {
+      wrap[0].innerHTML += `<div class="header--wrap--block"/>`;
+      blocks[i].style.animationDelay = `${i * 0.05}s`;
+    }
+  }, []);
+
   return (
     <header className="header">
-      <nav className="header--navber">
-        <div className="header--logo">AGRI</div>
-        <ul>
-          <li>sign up</li>
-          <li>sign in</li>
-        </ul>
-      </nav>
+      <Navbar />
       <div className="header--main">
         <div className="header--main--title">
-          <h1 className="header--main--title--text">title</h1>
+          <h1 className="header--main--title--text">Agriculture</h1>
         </div>
-        <div className="header--main--content">test</div>
+      </div>
+      <div className="header--wrap" ref={wrapRef}>
+        <div className="header--wrap--block" ref={blockRef} />
       </div>
     </header>
   );
